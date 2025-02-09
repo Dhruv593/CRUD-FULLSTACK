f6b3b3d0fba10d76420dd79ef4e96ce3b09cbf19
Dhruv Lad <dhruvlad4444@gmail.com>
2025-02-09 14:13:41 +0530

    env access config

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -1,7 +1,7 @@
 // src/api/userApi.js
 import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL ;
 
 if (!apiUrl) {
   console.error("API URL is not set. Check your .env file!");
